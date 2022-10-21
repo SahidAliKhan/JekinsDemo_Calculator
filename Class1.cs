bb83e669ac40b0a573f09a5a657747856c772eca
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-10-21 10:45:35 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -25,5 +25,9 @@ namespace JekinsDemo_Calculator
         {
             return num1 * num2;
         }
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
     }
 }
