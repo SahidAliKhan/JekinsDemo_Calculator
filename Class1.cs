e2dffa954804702694b1146a21305554a57f8bbe
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-10-21 09:35:38 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JekinsDemo_Calculator
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtract(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            }
+
+            return num2 - num1;
+        }
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
